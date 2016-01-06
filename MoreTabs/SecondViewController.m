a330f07d05b66c8b69970eae12dee6abd1821879
Don <donly@magicalboy.com>
2016-01-06 17:48:43 +0800

    Initial Commit

---
+++ MoreTabs/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  MoreTabs
+//
+//  Created by Yaokan-Don on 16/1/6.
+//  Copyright © 2016年 Dong. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
